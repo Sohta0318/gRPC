a6b64a273b8ee66fd5c0dd22d8fac6448fdf9b7f
SOHTA0318 <nicosohta0318@gmail.com>
2023-05-03 19:59:37 -0400

    lesson19 generate code based on proto file & acess generated code & start up server

---
+++ sec4 gRPC setup/greet/server/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	// get access generated proto file
+	// same as option in proto file
+	pb "github.com/Sohta0318/gRPC/greet/proto"
+	"google.golang.org/grpc"
+)
+
+type Server struct {
+	pb.GreetServiceServer
+}
+
+var addr string = "0.0.0.0:50051"
+
+func main() {
+	lis, err := net.Listen("tcp", addr)
+
+	if err != nil {
+		log.Fatalf("Failed to listen on: %v\n", err)
+	}
+
+	log.Printf("Listening on: %s\n", addr)
+
+	s := grpc.NewServer()
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("Failed to listen on: %v\n", err)
+	}
+}
